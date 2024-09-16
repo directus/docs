06e22a474bee5eb7dc5dd10008745d994e06a60e
Connor Winston <connor@winston.guru>
2024-09-15 20:28:41 -0500

    Remove Comment

--- server/utils/directus.ts
+++ server/utils/directus.ts
@@ -1,7 +1,6 @@
 import { createDirectus, rest } from '@directus/sdk';
 import type { DirectusSchema } from '~~/types/directus';
 
-// Client with REST support
 export const db = createDirectus<DirectusSchema>(
 	'https://product-team.directus.app',
 ).with(rest());
