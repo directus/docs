19dd41a9a3f15e016b9f684bfe2e1d5b8e3f678e
Connor Winston <connor@winston.guru>
2024-11-04 19:23:02 -0600

    fix: Tweak Engine Card

--- app/components/content/EngineStudioBox.vue
+++ app/components/content/EngineStudioBox.vue
@@ -47,7 +47,6 @@ const boxes = [
 					</NuxtLink>
 				</div>
 			</div>
-			<p>Get Started with a Quickstart</p>
 		</div>
 	</div>
 </template>
@@ -94,7 +93,7 @@ const boxes = [
 	background-position: center top;
 	border: 1px solid var(--border);
 	border-radius: calc(var(--border-radius) * 2);
-	padding: 2rem;
+	padding: 1.5rem;
 	color: white;
 	display: flex;
 	flex-direction: column;
