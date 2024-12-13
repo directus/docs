ff6000ec6101c9ac243db68306d08b68c58f4122
Rijk van Zanten <rijkvanzanten@me.com>
2024-12-13 12:16:47 -0500

    Drop hero

--- app/components/DefaultLayout.vue
+++ app/components/DefaultLayout.vue
@@ -1,12 +1,7 @@
-<script setup lang="ts">
-const route = useRoute();
-</script>
-
 <template>
 	<div>
 		<HeaderBanner />
-		<HeaderHero v-if="route.path === '/' || route.path === '/getting-started/overview'" />
-		<HeaderHat v-else>
+		<HeaderHat>
 			<slot name="mobile-nav" />
 		</HeaderHat>
 		<HeaderNav />
