31f3eb97d04f1f1ab6df82752f0f0c41a8147f82
Rijk van Zanten <rijkvanzanten@me.com>
2024-12-16 10:30:26 -0500

    Bootstrap app

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+	</div>
+</template>
+
+<script setup lang="ts">
+// Your component logic goes here
+</script>
+
+<style scoped>
+/* Your component styles go here */
+</style>
---
+++ app/pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<!-- Your component template goes here -->
+	</div>
+</template>
+
+<script setup lang="ts">
+// Your component logic goes here
+</script>
+
+<style scoped>
+/* Your component styles go here */
+</style>
