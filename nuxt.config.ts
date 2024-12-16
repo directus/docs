e45fd3f69c40612e44b3054e31b349ee4f298c47
Rijk van Zanten <rijkvanzanten@me.com>
2024-12-16 11:16:44 -0500

    Setup prose components for in-md use

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,55 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	extends: ["@nuxt/ui-pro"],
+
 	compatibilityDate: "2024-11-01",
-	devtools: { enabled: true },
+
 	future: {
 		compatibilityVersion: 4,
 	},
-	extends: ["@nuxt/ui-pro"],
+
 	modules: ["@nuxt/eslint", "@nuxt/content", "@nuxt/ui"],
+
+	hooks: {
+		// Make all prose components available in content
+		"components:extend": (components) => {
+			const globals = components.filter((c) => {
+				return c.shortPath.startsWith('components/prose/');
+			});
+
+			globals.forEach((c) => c.global = true);
+		},
+	},
+
+	nitro: {
+		prerender: {
+			routes: ["/"],
+			crawlLinks: true,
+		},
+	},
+
+	devtools: {
+		enabled: true,
+	},
+
+	eslint: {
+		config: {
+			stylistic: {
+				indent: "tab",
+				semi: true,
+			},
+		},
+	},
+
 	icon: {
 		customCollections: [
 			{
-				prefix: 'directus',
-				dir: './app/assets/icons/products',
+				prefix: "directus",
+				dir: "./app/assets/icons/products",
 			},
 			{
-				prefix: 'frameworks',
-				dir: './app/assets/icons/frameworks',
+				prefix: "frameworks",
+				dir: "./app/assets/icons/frameworks",
 			},
 		],
 	},
