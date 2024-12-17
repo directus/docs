6ce7521425c43509f95f6e6622e3fd29f963ccbd
Rijk van Zanten <rijkvanzanten@me.com>
2024-12-17 12:38:31 -0500

    Partials back up

---
+++ app/components/content/Partial.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+	content: string;
+}>();
+</script>
+
+<template>
+	<ContentQuery v-slot="{ data }" :path="`/_partials/${content}`" find="one">
+		<ContentRenderer :value="data" />
+	</ContentQuery>
+</template>
