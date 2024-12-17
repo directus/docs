3637e1b0afd83b6e41ecb73f6a7e38c77544bfe3
Rijk van Zanten <rijkvanzanten@me.com>
2024-12-17 13:02:00 -0500

    Make opinions on shades

--- app/components/content/CtaCloud.vue
+++ app/components/content/CtaCloud.vue
@@ -5,7 +5,7 @@ const { cta } = useAppConfig();
 <template>
 	<NuxtLink
 		to="https://directus.cloud"
-		class="bg-gradient-to-r from-purple-500 to-pink-500 block pl-4 pr-6 py-4 rounded-md my-5 last:mb-0"
+		class="bg-gradient-to-tr from-purple-500 to-pink-500 block pl-4 pr-6 py-4 rounded-md my-5 last:mb-0"
 	>
 		<LogoCloud class="h-8 w-auto" />
 		<span class="text-white text-sm my-2 block">{{ cta.cloud.description }}</span>
