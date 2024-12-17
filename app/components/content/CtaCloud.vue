140d5bdf542f3ab2bb91d7a4dc8e2683b573f2f4
Rijk van Zanten <rijkvanzanten@me.com>
2024-12-17 13:04:31 -0500

    Tweak cta banner some more

--- app/components/content/CtaCloud.vue
+++ app/components/content/CtaCloud.vue
@@ -5,10 +5,12 @@ const { cta } = useAppConfig();
 <template>
 	<NuxtLink
 		to="https://directus.cloud"
-		class="bg-gradient-to-tr from-purple-500 to-pink-500 block pl-4 pr-6 py-4 rounded-md my-5 last:mb-0"
+		class="flex justify-between items-center bg-gradient-to-tr from-purple-500 to-pink-500 pl-4 pr-6 py-4 rounded-md my-5 last:mb-0"
 	>
-		<LogoCloud class="h-8 w-auto" />
-		<span class="text-white text-sm my-2 block">{{ cta.cloud.description }}</span>
-		<UButton class="block" color="white">{{ cta.cloud.cta }}</UButton>
+		<span>
+			<LogoCloud class="h-8 w-auto" />
+			<span class="text-white text-sm mt-2 block">{{ cta.cloud.description }}</span>
+		</span>
+		<UButton class="block shrink-0" size="lg" color="white">{{ cta.cloud.cta }}</UButton>
 	</NuxtLink>
 </template>
