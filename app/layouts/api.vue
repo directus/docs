08b7ce5ef88ddc75660f3e324696c5908b2d4a85
Rijk van Zanten <rijkvanzanten@me.com>
2024-12-17 19:43:27 -0500

    Render api full width

--- app/layouts/api.vue
+++ app/layouts/api.vue
@@ -5,21 +5,19 @@ const openapi = inject<OpenAPIObject>('openapi')!;
 </script>
 
 <template>
-	<div>
-		<UContainer>
-			<UPage>
-				<template #left>
-					<UAside>
-						<UNavigationTree
-							:links="mapOasNavigation(openapi)"
-							:multiple="false"
-							default-open
-						/>
-					</UAside>
-				</template>
+	<div class="mx-6">
+		<UPage>
+			<template #left>
+				<UAside>
+					<UNavigationTree
+						:links="mapOasNavigation(openapi)"
+						:multiple="false"
+						default-open
+					/>
+				</UAside>
+			</template>
 
-				<slot />
-			</UPage>
-		</UContainer>
+			<slot />
+		</UPage>
 	</div>
 </template>
