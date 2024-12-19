e02efc4ee2ae5c5a7807da93bd6fd3b13ee1a2a2
Rijk van Zanten <rijkvanzanten@me.com>
2024-12-19 18:12:54 -0500

    Update footer copyright contrast

--- app/components/DocsFooter.vue
+++ app/components/DocsFooter.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 const { footer } = useAppConfig();
+const route = useRoute();
 </script>
 
 <template>
 	<UFooter
 		:links="footer.links"
 		class="border-t border-gray-200 dark:border-gray-800"
-		:ui="$route.path.startsWith('/api') ? {bottom: {container: 'max-w-screen'}} : {}"
+		:ui="route.path.startsWith('/api') ? { bottom: { container: 'max-w-screen' } } : {}"
 	>
 		<template #left>
-			<p class="text-sm font-semibold text-gray-700 dark:text-gray-300">
+			<p class="text-sm text-gray-400 dark:text-gray-500">
 				©2024 Monospace Inc
 			</p>
 		</template>
