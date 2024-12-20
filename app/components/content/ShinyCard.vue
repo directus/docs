39b2d72dfe0d75f0cd7f992ec43b4e2d01c7a069
Rijk van Zanten <rijkvanzanten@me.com>
2024-12-20 15:30:36 -0500

    Fix margin shenanigans

--- app/components/content/ShinyCard.vue
+++ app/components/content/ShinyCard.vue
@@ -1,14 +1,12 @@
 <template>
 	<ULandingCard
 		v-bind="$attrs"
-		class="before:blur-sm before:opacity-50"
-		:ui="{
-			title: '!my-0',
-		}"
+		class="before:blur-sm before:opacity-50 not-prose"
 	>
 		<template #title>
 			{{ $attrs.title }}
 		</template>
+
 		<slot />
 	</ULandingCard>
 </template>
