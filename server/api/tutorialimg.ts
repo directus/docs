86d0d4b8d1f8ed83b24ad701793d19fba7dafd2e
Carmen Huidobro <hola@carmenh.dev>
2025-04-23 14:03:03 +0200

    Serve the background for now

--- server/api/tutorialimg.ts
+++ server/api/tutorialimg.ts
@@ -1,5 +1,39 @@
-export default defineEventHandler((event) => {
-  return {
-    hello: 'world'
-  }
-})
+// import { readFileSync } from "fs";
+import { join } from "path";
+import sharp from "sharp";
+
+export default defineEventHandler(async (event) => {
+	const baseImagePath = join('public/img/tutorials', 'background.png');
+	// const icon1Path = join('public/images', 'icon1.png')
+	// const icon2Path = join('public/images', 'icon2.png')
+
+	// Load base image
+	const base = sharp(baseImagePath);
+
+	// // Prepare icon overlays
+	// const icon1Buffer = readFileSync(icon1Path)
+	// const icon2Buffer = readFileSync(icon2Path)
+
+	// const compositeImages = [
+	//   {
+	//     input: icon1Buffer,
+	//     top: 50,
+	//     left: 100,
+	//   },
+	//   {
+	//     input: icon2Buffer,
+	//     top: 300,
+	//     left: 400,
+	//   },
+	// ]
+
+	// Compose the image
+	const finalImage = await base
+	// .composite(compositeImages)
+		.jpeg()
+		.toBuffer();
+
+	// Serve it
+	setHeader(event, 'Content-Type', 'image/jpeg');
+	return finalImage;
+});
