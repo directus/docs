5a9838a2bde974aae5e9689e350ab5909f2b88a5
Carmen Huidobro <hola@carmenh.dev>
2025-04-28 13:20:18 +0200

    Grab the first logo

--- server/api/tutorialimg.ts
+++ server/api/tutorialimg.ts
@@ -3,12 +3,14 @@ import { join } from "path";
 import sharp from "sharp";
 
 export default defineEventHandler(async (event) => {
-  const query = getQuery(event);
-	const logoFileName = query.logo || "directus"; // default to directus.png
+	const query = getQuery(event);
+	const logoFileNames = query?.logos?.split(', ') || ['directus']; // default to directus.png
+	const logoFileName = logoFileNames[0];
+  console.log('logoFileName', logoFileName);
 	const baseImagePath = join('public/img/tutorials', 'background.png');
 	const logoContainerPath = join('public/img/tutorials', 'logo-container.png');
 	// const astroLogoPath = join('public/img/tutorials', 'astro.png');
-  const logoPath = join('public/img/tutorials', `${logoFileName}.png`);
+	const logoPath = join('public/img/tutorials', `${logoFileName}.png`);
 	// const directusLogoPath = join('public/img/tutorials', 'directus.png');
 
 	// Load buffers
@@ -28,17 +30,17 @@ export default defineEventHandler(async (event) => {
 	const top = Math.floor((baseMetadata.height! - logoMetadata.height!) / 2);
 
 	const compositeImages = [
-	  {
-	    input: logoContainerBuffer,
-	    top,
-	    left,
-	  },
-	  {
+		{
+			input: logoContainerBuffer,
+			top,
+			left,
+		},
+		{
 
-	    input: logoBuffer,
-	  	top: Math.floor(top + (logoMetadata.height! - directusMetadata.height!) / 2),
-	    left: Math.floor(left + (logoMetadata.width! - directusMetadata.width!) / 2),
-	  }
+			input: logoBuffer,
+			top: Math.floor(top + (logoMetadata.height! - directusMetadata.height!) / 2),
+			left: Math.floor(left + (logoMetadata.width! - directusMetadata.width!) / 2),
+		}
 	];
 
 	// Compose the image
