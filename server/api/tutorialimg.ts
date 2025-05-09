f428d24a576611bb79c2f37392125b776efb8b1e
Carmen Huidobro <hola@carmenh.dev>
2025-05-09 11:57:00 +0200

    Use HTTP requests

--- server/api/tutorialimg.ts
+++ server/api/tutorialimg.ts
@@ -2,25 +2,30 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import sharp from "sharp";
 
+async function getImageBuffer(baseURL: string, imagePath: string) {
+	const imageUrl = `${baseURL}/img/${imagePath}.png`;
+
+	const res = await fetch(imageUrl);
+	if (!res.ok) {
+		throw createError({ statusCode: 404, statusMessage: 'Image not found' });
+	}
+
+	return Buffer.from(await res.arrayBuffer());
+}
+
 export default defineEventHandler(async (event) => {
 	const query = getQuery(event);
 	const logoFileNames = query?.logos?.split(', ') || ['directus']; // default to directus.png
 	const logoFileName = logoFileNames[0];
-	const baseImagePath = join(process.cwd(), 'public/img/tutorials/background.png');
-	const logoContainerPath = join(process.cwd(), 'public/img/tutorials/logo-container.png');
+	const baseImageBuffer = await getImageBuffer(getRequestURL(event).origin, 'background.png');
+	const logoContainerBuffer = await getImageBuffer(getRequestURL(event).origin, 'logo-container.png');
 	// const astroLogoPath = join('public/img/tutorials', 'astro.png');
-	const logoPath = join(process.cwd(), `public/img/tutorials/${logoFileName}.png`);
+	const logoBuffer = await getImageBuffer(getRequestURL(event).origin, `${logoFileName}.png`);
 	// const directusLogoPath = join('public/img/tutorials', 'directus.png');
-
-	// Load buffers
-	const base = sharp(baseImagePath);
-	const logoContainerBuffer = readFileSync(logoContainerPath);
-	// const astroLogoBuffer = readFileSync(astroLogoPath);
-	const logoBuffer = readFileSync(logoPath);
 
 	// Get metadata to center the logo container
 	const [baseMetadata, logoMetadata, directusMetadata] = await Promise.all([
-		base.metadata(),
+		sharp(baseImageBuffer).metadata(),
 		sharp(logoContainerBuffer).metadata(),
 		sharp(logoBuffer).metadata(),
 	]);
